d7bf9fce68ccd072628d6e520b4c2c5fb7551737
Viraj Lakshitha Bandara <62491019+viraj-lakshitha@users.noreply.github.com>
2022-10-16 17:35:44 +0530

    Implement basic product page layout with mockdata

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/login/Login";
+import Products from "./pages/products/Products";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </BrowserRouter>
   );
